8e513dc3e8b4f515754230ee7c33b5453c3293a7
digu-dev <rodrigo.dev.contact@gmail.com>
2025-12-02 15:37:28 -0300

    Update connection

--- src/main/java/com/github/digu_dev/schoolapi/SchoolapiApplication.java
+++ src/main/java/com/github/digu_dev/schoolapi/SchoolapiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class SchoolapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(SchoolapiApplication.class, args);
 	}
 
 }
